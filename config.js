23032b4ba8744de756ef50da111bbcf60e9b756d
Ravi Deevan <ravideevan3@gmail.com>
2022-09-24 08:46:19 +0800

    Remove cards and lists

--- config.js
+++ config.js
@@ -11,26 +11,26 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'excalios',
+  name: "excalios",
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Go to Sleep!",
 
   // Weather
-  weatherKey: '8aee6ae51e2da374c2e74e25483499ec',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  weatherKey: "8aee6ae51e2da374c2e74e25483499ec",
+  weatherIcons: "Nord", // 'Nord', 'Dark', 'White'
+  weatherUnit: "C", // 'F', 'C'
+  language: "en", // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '-3.318607',
-  defaultLongitude: '114.594376',
+  defaultLatitude: "-3.318607",
+  defaultLongitude: "114.594376",
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -39,40 +39,40 @@ const CONFIG = {
   // Links
   cards: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/",
     },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.google.com/',
+      id: "2",
+      name: "Mail",
+      icon: "mail",
+      link: "https://mail.google.com/",
     },
     {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://trello.com',
+      id: "3",
+      name: "Todoist",
+      icon: "trello",
+      link: "https://trello.com",
     },
     {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '5',
-      name: 'Reddit',
-      icon: 'bookmark',
-      link: 'https://reddit.com',
+      id: "5",
+      name: "Reddit",
+      icon: "bookmark",
+      link: "https://reddit.com",
     },
     {
-      id: '6',
-      name: 'YouTube',
-      icon: 'youtube',
-      link: 'https://youtube.com/',
+      id: "6",
+      name: "YouTube",
+      icon: "youtube",
+      link: "https://youtube.com/",
     },
   ],
 
@@ -81,45 +81,45 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: "music",
+  secondListIcon: "coffee",
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Inspirational",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Classic",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Oldies",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: "Rock",
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: "Linkedin",
+        link: "https://linkedin.com/",
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: "Figma",
+        link: "https://figma.com/",
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: "Dribbble",
+        link: "https://dribbble.com",
       },
       {
-        name: 'WhatsApp',
-        link: 'https://web.whatsapp.com',
+        name: "WhatsApp",
+        link: "https://web.whatsapp.com",
       },
     ],
   },
